a3f9d0c1f899d2d0a0b383a538d36be415a1586c
AngPoll <apollastrone@gmail.com>
2026-02-25 12:37:45 -0500

    Create StockChart.tsx

---
+++ components/StockChart.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function StockChart() {
+  return (
+    <div style={{ padding: 16, border: "1px solid #ccc", borderRadius: 8 }}>
+      StockChart component placeholder
+    </div>
+  );
+}
